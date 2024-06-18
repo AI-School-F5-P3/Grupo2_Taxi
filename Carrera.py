4b8d06e8be2814302e4a8ff6d9a4fc7f8f81b49b
SergioSierraMelero <sergiosm365@gmail.com>
2024-06-18 15:58:06 +0200

    Update Carrera.py
    Quito una coma sobrante en "precio_movimiento"

--- Carrera.py
+++ Carrera.py
@@ -2,7 +2,7 @@ import datetime
 
 class Carrera:
     precio_parada = 0.02
-    precio_movimiento = 0,.05
+    precio_movimiento = 0.05
     precio_parada_nocturno = precio_parada * 2
     precio_movimiento_nocturno = precio_movimiento * 2
     hora_inicio = 0
