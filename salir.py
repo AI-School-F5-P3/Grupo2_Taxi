de0c647f6013eef16b1e39bb9306dc2b7f877cab
Krol_Ina <162979572+KrolinaTF@users.noreply.github.com>
2024-06-19 15:18:43 +0200

    Add files via upload

--- salir.py
+++ salir.py
@@ -1,5 +1,10 @@
 
 def salir(self):
-    elif option == 6:
+     while True:
+        
+        confirmacion = input('¿seguro que deseas cerrar? Si estás seguro pulsa de nuevo 3')
+        if confirmacion == 3:
             print('Sales de la aplicación. Hasta pronto')
             break
+        else:
+            def mostrar_menu():
