e471da488eeb6fdd97561dee0289d08766f54f2d
Krol_Ina <carolinatomasf@gmail.com>
2024-06-24 11:32:07 +0200

    quitar mensajes que no hacen nada

--- app_taxi/main.py
+++ app_taxi/main.py
@@ -7,9 +7,6 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
                     filename = 'log_taxi.log', 
                     filemode = 'a')
-logging.warning('ha habido un problema nivel warning')
-logging.error('ha habido un problema nivel error')
-logging.critical('ha habido un problema nivel critical')
 saludar.saludar()
 entrar_con_password.entrar_con_password()
 mostrar_menu.mostrar_menu()
