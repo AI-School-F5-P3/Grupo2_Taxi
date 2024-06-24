f4f5905c92bd71346924c4c2cfd6a0b4c6104fb4
Krol_Ina <162979572+KrolinaTF@users.noreply.github.com>
2024-06-24 10:06:52 +0200

    logging agregado

--- app_taxi/main.py
+++ app_taxi/main.py
@@ -1,7 +1,12 @@
 import saludar
 import entrar_con_password
 import mostrar_menu
-
+import datetime
+import logging
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+                    filename = 'log_taxi.log', 
+                    filemode = 'a')
 saludar.saludar()
 entrar_con_password.entrar_con_password()
 mostrar_menu.mostrar_menu()
