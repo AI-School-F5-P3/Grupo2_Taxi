efdaed5b683a82b4f29fea1285dcbe0bd73552c1
Krol_Ina <carolinatomasf@gmail.com>
2024-06-24 14:01:32 +0200

    cambios log

--- app_taxi/main.py
+++ app_taxi/main.py
@@ -1,7 +1,6 @@
 import saludar
 import entrar_con_password
 import mostrar_menu
-import datetime
 import logging
 import sys
 logging.basicConfig(level=logging.DEBUG, 
@@ -12,6 +11,7 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     logging.error("excepcion no recogida", exc_info=(exc_type, exc_value, exc_traceback))
 
 sys.excepthook = handle_exception
+logger = logging.getLogger(__name__)
 saludar.saludar()
 entrar_con_password.entrar_con_password()
 mostrar_menu.mostrar_menu()
