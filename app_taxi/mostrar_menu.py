76b250ac1f9e6326b2af36a706f9f20e8cd2ff53
SergioSierraMelero <sergiosm365@gmail.com>
2024-06-24 10:53:09 +0200

    Reparo fallo al salir

--- app_taxi/mostrar_menu.py
+++ app_taxi/mostrar_menu.py
@@ -88,9 +88,9 @@ def mostrar_menu():
             mensaje = 'Vas a salir de la aplicación'
             while True:
         
-                confirmacion = int(input('¿seguro que deseas cerrar? Si estás seguro pulsa de nuevo 3 '))
+                confirmacion = input('¿seguro que deseas cerrar? Si estás seguro pulsa de nuevo 3 ')
         
-                if confirmacion != 3:
+                if confirmacion != "3":
                     mostrar_menu()
                 else:
                     print('Sales de la aplicación. Hasta pronto')
