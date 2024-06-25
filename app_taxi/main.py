c39977813ab88327221841b265c192f5db890e82
Krol_Ina <carolinatomasf@gmail.com>
2024-06-25 09:19:05 +0200

    un cambio de orden

--- app_taxi/main.py
+++ app_taxi/main.py
@@ -4,10 +4,6 @@ import entrar_con_password
 import logging
 import sys 
 
-saludar.saludar()  # Saluda al usuario al inicio del programa
-entrar_con_password.menu_principal()  # Intenta iniciar sesión primero
-    
-    # Después del inicio de sesión exitoso, muestra el menú principal
 logging.basicConfig(level=logging.DEBUG, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
                     filename = 'log_taxi.log', 
@@ -15,5 +11,10 @@ logging.basicConfig(level=logging.DEBUG,
 def handle_exception(exc_type, exc_value, exc_traceback):
     logging.error("excepcion no recogida", exc_info=(exc_type, exc_value, exc_traceback))
 sys.excepthook = handle_exception
+
+saludar.saludar()  # Saluda al usuario al inicio del programa
+entrar_con_password.menu_principal()  # Intenta iniciar sesión primero
+    
+    # Después del inicio de sesión exitoso, muestra el menú principal
 
 mostrar_menu.mostrar_menu()
