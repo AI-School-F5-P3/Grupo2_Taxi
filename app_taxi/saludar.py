688f69d80bef13c0011a5b47f16a32f554a81717
SergioSierraMelero <sergiosm365@gmail.com>
2024-06-27 15:16:17 +0200

    soluciono errores menores

--- app_taxi/saludar.py
+++ app_taxi/saludar.py
@@ -2,7 +2,7 @@ import logging
 
 def saludar():
     print(
-        '¡Bienvenido a Taxi Meter!: \n'
+        '¡Bienvenido a Taxiter!: \n'
         'Inicia sesión con tu usuario y contraseña para acceder a la aplicación. \n'
         'En el MENÚ podrás iniciar una carrera, cambiar de usuario o incluso modificar las tarifas. \n'
         'Con la carrera iniciada el taxi está arrancado, así que cobrará 0.02 céntimos por segundo.\n'
